9e0ce2fab0ab08c279b737252899026fe6865a30
LaviJ <lavinajagwani@gmail.com>
2021-10-12 16:45:16 -0700

    Completion of Module 9.4.3

---
+++ app.py
@@ -0,0 +1,21 @@
+# import the dependency, will enable your code to access all that Flask has to offer
+# import the Flask dependency
+from flask import Flask
+
+# create a new Flask app instance. "Instance" is a general term in programming to refer to a singular version of something
+app = Flask(__name__)
+
+# Create Flask Routes
+#@app.route('/')
+
+# create a function called hello_world(). Whenever you make a route in Flask, you put the code you want in that specific route below @app.route()
+@app.route('/')
+def hello_world():
+    return "Hello world"
+
+# Run a Flask App
+#export FLASK_APP=app.py
+
+#set FLASK_APP=app.py
+
+#flask run
